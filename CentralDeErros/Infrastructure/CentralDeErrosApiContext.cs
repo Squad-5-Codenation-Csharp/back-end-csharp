5a779dcebc1c544cf01d53e81036828780c5884b
Elias Bernardo <ebmm01@gmail.com>
2020-07-29 21:56:54 -0300

    Adding AutoMapper and fixing DbContext
    Co-authored-by: Gustavo Carvalho <gustavocarvalho1002@gmail.com>

---
+++ CentralDeErros/Infrastructure/CentralDeErrosApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CentralDeErros.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CentralDeErros.Api.Data
+{
+    public class CentralDeErrosApiContext : DbContext
+    {
+        public CentralDeErrosApiContext (DbContextOptions<CentralDeErrosApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Log> Log { get; set; }
+    }
+}
--- CentralDeErros/Startup.cs
+++ CentralDeErros/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AutoMapper;
 
 namespace CentralDeErros
 {
@@ -26,6 +27,7 @@ namespace CentralDeErros
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

2507abddf3bfb8f052217f9b560b0268787ebbdf
Elias Bernardo <ebmm01@gmail.com>
2020-08-04 11:31:56 -0300

    Adding log controller, response model and request validation

---
+++ CentralDeErros/Controllers/LogController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CentralDeErros.Api.Models;
+using CentralDeErros.RequestValidations;
+using CentralDeErros.ResponseModel;
+using CentralDeErros.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralDeErros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private ILogService service;
+        private IMapper mapper;
+
+        public LogController(ILogService service, IMapper mapper)
+        {
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IList<LogResponseModel>> GetAll()
+        {
+            var logList = service.GetAll();
+
+            return Ok(mapper.Map<IList<LogResponseModel>>(logList));
+        }
+
+        [HttpPost]
+        public ActionResult<int> Post([FromBody] CreateLogRequestValidation log)
+        {
+            var UserId = service.Save(mapper.Map<Log>(log));
+
+            return Ok(UserId);
+        }
+    }
+}
---
+++ CentralDeErros/RequestValidations/CreateLogRequestValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.RequestValidations
+{
+    public class CreateLogRequestValidation
+    {
+        [Required(ErrorMessage = "É necessário informar o nome do log")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar a descrição do log")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar o ambiente do log")]
+        public string Environment { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar o tipo do log")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar o id do usuário")]
+        public int UserId { get; set; }
+    }
+}

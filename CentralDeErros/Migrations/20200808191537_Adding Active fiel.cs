f0cd5de87062c6df1abc91cdc3ef3378d6f4864c
Elias Bernardo <ebmm01@gmail.com>
2020-08-08 17:10:33 -0300

    Adding new migrations

---
+++ CentralDeErros/Migrations/20200808191537_Adding Active fiel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CentralDeErros.Migrations
+{
+    public partial class AddingActivefiel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "User");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "User",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Log",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Log");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}

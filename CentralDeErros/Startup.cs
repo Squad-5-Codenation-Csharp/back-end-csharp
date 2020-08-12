1edc27b0368b5a7c0a95e029dd6aa6f3b030f00c
Elias Bernardo <ebmm01@gmail.com>
2020-08-12 11:45:41 -0300

    Configuring authentication on startup and adding necessary packages

--- CentralDeErros/Startup.cs
+++ CentralDeErros/Startup.cs
@@ -18,6 +18,10 @@ using CentralDeErros.Services;
 using CentralDeErros.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using CentralDeErros.Data.Interfaces;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.InteropServices;
 
 namespace CentralDeErros
 {
@@ -33,6 +37,7 @@ namespace CentralDeErros
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<IUserRepository, UserRepository>();
@@ -44,6 +49,26 @@ namespace CentralDeErros
             services.AddDbContext<CentralDeErrosApiContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("CentralDeErrosApiContext")));
             services.AddSwaggerGen();
+
+            var key = Encoding.ASCII.GetBytes(Variables.JwtKey);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +91,13 @@ namespace CentralDeErros
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 

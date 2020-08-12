b8b0a4931d831ed963e5b73a7016e39860e45d4e
Elias Bernardo <ebmm01@gmail.com>
2020-08-12 10:48:29 -0300

    Adding Variables file

---
+++ CentralDeErros/Infrastructure/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.Infrastructure
+{
+    public static class Variables
+    {
+        public static string JwtKey = "f4bfa0ff9c52af05a42b097e677da924";
+        public static int TokenExpireTime = 2;
+    }
+}

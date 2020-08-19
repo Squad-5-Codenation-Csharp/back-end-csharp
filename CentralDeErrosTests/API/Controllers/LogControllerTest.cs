50664153b50dbccf08309c9ca343481b45be45f5
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2020-08-19 02:03:27 -0300

    Creating LogControllerTest

---
+++ CentralDeErrosTests/API/Controllers/LogControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CentralDeErros.Api.Models;
+using CentralDeErros.Controllers;
+using CentralDeErros.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CentralDeErrosTests.API.Controllers
+{
+    public class LogControllerTest
+    {
+        [Fact]
+        public void shoud_get_log_by_id()
+        {
+            var logServiceMock = new Mock<ILogService>();
+            var mapperMock = new Mock<IMapper>();
+
+            var log = new Log()
+            {
+                Id = 1,
+                Name = "Log_Name",
+                Description = "Log_Description",
+                Environment = "dev",
+                Type = "Error",
+                UserId = 1
+            };
+
+            logServiceMock.Setup(x => x.GetById(It.IsAny<int>())).Returns(log);
+
+            var userController = new LogController(logServiceMock.Object, mapperMock.Object);
+
+            var expectedUserList = userController.GetById(1);
+
+            var result = expectedUserList.Result as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+        }
+
+    }
+}
